48d0d338b473d3d710e428108bc53951aecdf958
player-geun <player_geun@naver.com>
2022-05-10 09:58:57 +0900

    item service #1 - item domain
    상품 도메인 개발을 진행한다. 
    
    테스트 코드를 작성하여 repository 가 잘 작동하는지 확인한다.

---
+++ src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -0,0 +1,42 @@
+package hello.itemservice.domain.item;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class ItemRepository {
+
+    private static final Map<Long, Item> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    public Item save(Item item) {
+        item.setId(++sequence);
+        store.put(item.getId(), item);
+
+        return item;
+    }
+
+    public Item findById(Long id) {
+        return store.get(id);
+    }
+
+    public List<Item> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void update(Long itemId, Item updateParam) {
+        Item findItem = findById(itemId);
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+    }
+    // update 를 item 에 있는 모든 프로퍼티를 업데이트 하지 않음 -> 따로 dto 빼서 만드는게 좋음
+
+    public void clearStore() {
+        store.clear();
+    }
+}
---
+++ src/test/java/hello/itemservice/domain/item/ItemRepositoryTest.java
@@ -0,0 +1,30 @@
+package hello.itemservice.domain.item;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemRepositoryTest {
+
+    ItemRepository itemRepository = new ItemRepository();
+
+    @AfterEach
+    void afterEach() {
+        itemRepository.clearStore();
+    }
+
+    @Test
+    void save() {
+        //given
+        Item item = new Item("itemA", 10000, 10);
+
+        //when
+        Item savedItem = itemRepository.save(item); // 테스트 목적
+
+        //then
+        Item findItem = itemRepository.findById(item.getId());
+        Assertions.assertThat(findItem).isEqualTo(savedItem);
+    }
+}
